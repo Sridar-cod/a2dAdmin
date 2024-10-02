297669ee22ee6f66595726fe307d727314e33e67
Sridar-cod <sridar9771@gmail.com>
2024-10-02 17:56:02 +0530

    ui changes

--- app/components/TopNav.js
+++ app/components/TopNav.js
@@ -4,7 +4,7 @@ const TopNav = () => {
   return (
     <>
       <div className="topNav d-flex justify-content-between align-items-center ps-4 pe-4 pt-2 pb-2">
-        <h2>Admin</h2>
+        <h3 className='gray-text'>Admin</h3>
         <b>admin name</b>
       </div>
     </>)
--- app/page.js
+++ app/page.js
@@ -1,15 +1,19 @@
+'use client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import Content from "./components/Content";
 import SideNav from "./components/SideNav";
 import TopNav from "./components/TopNav";
+import { useState } from "react";
 
 export default function Home() {
+  const [navWidth, setNavWidth] = useState(true);
+
   return (
     <>
       <TopNav />
       <div className="d-flex">
-        <SideNav />
-        <Content />
+        <SideNav setNavWidth={setNavWidth} navWidth={navWidth} />
+        <Content navWidth={navWidth} />
       </div>
     </>
   );
